26836c2cc972e014a927b2477c4aaff60fe3cb18
yeonx <71878202+yeonx@users.noreply.github.com>
2023-07-19 16:23:35 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/ssafy/campfire/CampfireApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.campfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/campfire/CampfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.campfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
