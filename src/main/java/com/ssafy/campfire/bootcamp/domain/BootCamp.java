8cab69fdc319e3073cf10f207367883deda79201
dksek3050 <dksek3050@gmail.com>
2023-07-21 13:58:40 +0900

    feat: BootCamp 엔티티 구현

---
+++ src/main/java/com/ssafy/campfire/bootcamp/domain/BootCamp.java
@@ -0,0 +1,52 @@
+package com.ssafy.campfire.bootcamp.domain;
+
+import com.ssafy.campfire.utils.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor //기본 생성자 생성
+@Entity
+@Table(name = "bootcamp") // DB 이름과 동일하게
+public class BootCamp extends BaseEntity {
+    //field
+    @Id //부트캠프 테이블의 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 자동생성 - identity는 기본키 자동생성 방법을 데이터베이스에 위임함.
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "site_url")
+    private String siteUrl;
+
+    private String procedure;
+
+    private String schedule;
+
+    private String description;
+
+    private Double cost;
+
+    private Boolean card;
+
+    private Boolean support;
+
+    @Column(name = "has_codingtest")
+    private Boolean hasCodingtest;
+
+    @Column(name ="review_cnt", nullable = false)
+    private Integer reviewCnt;
+
+    @Column(name = "total_score")
+    private Double totalScore;
+
+    @Column(name ="algo_cnt",nullable = false)
+    private Integer algoCnt;
+
+
+
+}
