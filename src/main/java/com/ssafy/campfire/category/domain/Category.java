e6d3de49d7db7a1c993ba28f7d057b916cf2a616
yeonx <ydol2@naver.com>
2023-07-21 12:53:58 +0900

    feat: category 엔티티 구현

--- src/main/java/com/ssafy/campfire/category/domain/Category.java
+++ src/main/java/com/ssafy/campfire/category/domain/Category.java
@@ -1,11 +1,14 @@
 package com.ssafy.campfire.category.domain;
 
 import com.ssafy.campfire.category.domain.enums.CategoryType;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +26,7 @@ public class Category {
     private CategoryType name;
 
     // 부트캠프 id join 해야함.
-
+//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JoinColumn(name = "bootcamp_id")
+//    private Bootcamp bootcamp;
 }
