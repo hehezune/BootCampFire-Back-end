55f1d43f4239f106bc95827f408c81c6180bad2c
yeonx <ydol2@naver.com>
2023-07-24 10:15:23 +0900

    feat: 카테고리별 조회

--- src/main/java/com/ssafy/campfire/category/repository/CustomCategoryRepository.java
+++ src/main/java/com/ssafy/campfire/category/repository/CustomCategoryRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.campfire.category.repository;
 
 import com.ssafy.campfire.board.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomCategoryRepository {
     List<Board> getLatestFiveBoard(Long categoryId);
+    Page<Board> getBoardByNewest(Long categoryId, Pageable pageable);
+    Page<Board> getBoardByLike(Long categoryId, Pageable pageable);
+    Page<Board> getBoardByView(Long categoryId, Pageable pageable);
 }
--- src/main/java/com/ssafy/campfire/category/repository/impl/CustomCategoryRepositoryImpl.java
+++ src/main/java/com/ssafy/campfire/category/repository/impl/CustomCategoryRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.ssafy.campfire.category.repository.impl;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.campfire.board.domain.Board;
 import com.ssafy.campfire.category.repository.CustomCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,11 +23,52 @@ public class CustomCategoryRepositoryImpl implements CustomCategoryRepository {
 //    //user, likes, comment join 필요
     @Override
     public List<Board> getLatestFiveBoard(Long categoryId) {
-        return queryFactory.selectFrom(board)
+        return queryFactory.select(board)
+                .from(board)
                 .leftJoin(board.category, category)
                 .where(board.category.id.eq(categoryId))
                 .orderBy(board.createdDate.desc())
                 .limit(5)
                 .fetch();
     }
+
+    @Override
+    public Page<Board> getBoardByNewest(Long categoryId, Pageable pageable) {
+        return (Page<Board>) queryFactory.select(board)
+                .from(board)
+                .leftJoin(board.category, category)
+                .fetchJoin()
+                .where(board.category.id.eq(categoryId))
+                .orderBy(board.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public Page<Board> getBoardByLike(Long categoryId, Pageable pageable) {
+        return (Page<Board>) queryFactory.select(board)
+                .from(board)
+                .leftJoin(board.category, category)
+                .fetchJoin()
+                .where(board.category.id.eq(categoryId))
+                .orderBy(board.likeCnt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public Page<Board> getBoardByView(Long categoryId, Pageable pageable) {
+        return (Page<Board>) queryFactory.select(board)
+                .from(board)
+                .leftJoin(board.category, category)
+                .fetchJoin()
+                .where(board.category.id.eq(categoryId))
+                .orderBy(board.view.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+    
 }
