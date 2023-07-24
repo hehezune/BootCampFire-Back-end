f818cb6206dc1681b6cc99f65925103a20f9d977
yeonx <ydol2@naver.com>
2023-07-24 12:57:04 +0900

    feat: custom 예외처리 구현

--- src/main/java/com/ssafy/campfire/utils/error/dto/ErrorResponseDto.java
+++ src/main/java/com/ssafy/campfire/utils/error/dto/ErrorResponseDto.java
@@ -1,4 +1,4 @@
-package com.ssafy.campfire.utils.exception.dto;
+package com.ssafy.campfire.utils.error.dto;
 
 public record ErrorResponseDto(int code, String phrase) {
     public static ErrorResponseDto of(int code,String phrase){
--- src/main/java/com/ssafy/campfire/utils/error/enums/ErrorMessage.java
+++ src/main/java/com/ssafy/campfire/utils/error/enums/ErrorMessage.java
@@ -1,4 +1,4 @@
-package com.ssafy.campfire.utils.exception.enums;
+package com.ssafy.campfire.utils.error.enums;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
