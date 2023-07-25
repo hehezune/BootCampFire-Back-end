2b0de0df9e5594b2de2122bd3d3c0b0c1e2f4fbb
yeonx <ydol2@naver.com>
2023-07-25 14:56:43 +0900

    feat: 게시글 로직 설

---
+++ src/main/java/com/ssafy/campfire/board/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.ssafy.campfire.board.service;
+
+import com.ssafy.campfire.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+}
