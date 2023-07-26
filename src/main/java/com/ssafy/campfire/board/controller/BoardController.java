eff231551a2189c020b4b66c01a1e064b1e99457
yeonx <ydol2@naver.com>
2023-07-26 12:46:07 +0900

    feat: 게시글 수정 API

--- src/main/java/com/ssafy/campfire/board/controller/BoardController.java
+++ src/main/java/com/ssafy/campfire/board/controller/BoardController.java
@@ -1,17 +1,17 @@
 package com.ssafy.campfire.board.controller;
 
+import com.ssafy.campfire.board.domain.dto.BoardUpdate;
 import com.ssafy.campfire.board.dto.request.BoardCreateRequest;
+import com.ssafy.campfire.board.dto.request.BoardUpdateRequest;
 import com.ssafy.campfire.board.dto.response.BoardCreateResponse;
+import com.ssafy.campfire.board.dto.response.BoardUpdateResponse;
 import com.ssafy.campfire.board.service.BoardService;
 import com.ssafy.campfire.user.domain.User;
 import com.ssafy.campfire.utils.dto.response.BaseResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -22,12 +22,19 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @ApiOperation(value ="게시글 수정")
+    @PutMapping("/{boardId}")
+    public BaseResponseDto<BoardUpdateResponse> updateBoard(@PathVariable Long boardId,
+                                                            @RequestBody @Valid BoardUpdateRequest request) {
+        return BaseResponseDto.ok(boardService.update(boardId, request));
+    }
+
     /**
      * @AuthenticationPrincipal 설정 후
      */
 //    @ApiOperation(value ="게시글 저장")
 //    @PostMapping
-//    public BaseResponseDto<BoardCreateResponse> createPost(@RequestBody @Valid BoardCreateRequest request,
+//    public BaseResponseDto<BoardCreateResponse> createBoard(@RequestBody @Valid BoardCreateRequest request,
 //                                                           @AuthenticationPrincipal User user) {
 //        return BaseResponseDto.ok(boardService.save(user.getId(), request));
 //    }
@@ -37,7 +44,7 @@ public class BoardController {
      */
     @ApiOperation(value ="게시글 저장")
     @PostMapping
-    public BaseResponseDto<BoardCreateResponse> createPost(@RequestBody @Valid BoardCreateRequest request,
+    public BaseResponseDto<BoardCreateResponse> createBoard(@RequestBody @Valid BoardCreateRequest request,
                                                            Long userId) {
         return BaseResponseDto.ok(boardService.save(userId, request));
     }
