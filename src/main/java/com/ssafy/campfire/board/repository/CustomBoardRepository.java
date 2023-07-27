da473845f92703b15b41299eb2ffb2f2e8915afa
yeonx <ydol2@naver.com>
2023-07-27 14:01:42 +0900

    feat: 사용자 글 가져오기 쿼리

--- src/main/java/com/ssafy/campfire/board/repository/CustomBoardRepository.java
+++ src/main/java/com/ssafy/campfire/board/repository/CustomBoardRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.campfire.board.repository;
 
 import com.ssafy.campfire.board.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface CustomBoardRepository {
+    Page<Board> getUserBoard(Long userId, Pageable pageable);
     Optional<Board> getByIdFetchJoin(Long boardId);
 }
--- src/main/java/com/ssafy/campfire/board/repository/impl/CustomBoardRepositoryImpl.java
+++ src/main/java/com/ssafy/campfire/board/repository/impl/CustomBoardRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.ssafy.campfire.board.repository.impl;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.campfire.board.domain.Board;
 import com.ssafy.campfire.board.repository.CustomBoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.ssafy.campfire.board.domain.QBoard.board;
@@ -30,5 +35,31 @@ public class CustomBoardRepositoryImpl implements CustomBoardRepository {
                         .where(board.id.eq(boardId))
                         .fetchOne()
         );
+    }
+
+    @Override
+    public Page<Board> getUserBoard(Long userId, Pageable pageable) {
+
+        List<Board> boards = queryFactory.select(board)
+                .from(board)
+                .leftJoin(board.category, category)
+                .fetchJoin()
+                .leftJoin(board.user, user)
+                .fetchJoin()
+                .where(board.user.id.eq(userId))
+                .orderBy(board.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory.select(board.count())
+                .from(board)
+                .leftJoin(board.category, category)
+                .fetchJoin()
+                .leftJoin(board.user, user)
+                .fetchJoin()
+                .where(board.user.id.eq(userId));
+
+        return PageableExecutionUtils.getPage(boards, pageable, countQuery::fetchOne);
     }
 }
