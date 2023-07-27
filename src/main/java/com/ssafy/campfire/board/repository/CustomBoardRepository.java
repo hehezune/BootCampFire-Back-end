0e1f0b9600381840f50bab5d618efdcd54017a19
yeonx <ydol2@naver.com>
2023-07-27 13:42:58 +0900

    feat: 게시글 상세 조회 로직

--- src/main/java/com/ssafy/campfire/board/repository/CustomBoardRepository.java
+++ src/main/java/com/ssafy/campfire/board/repository/CustomBoardRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.campfire.board.repository;
 
+import com.ssafy.campfire.board.domain.Board;
+
+import java.util.Optional;
+
 public interface CustomBoardRepository {
+    Optional<Board> getByIdFetchJoin(Long boardId);
 }
--- src/main/java/com/ssafy/campfire/board/repository/impl/CustomBoardRepositoryImpl.java
+++ src/main/java/com/ssafy/campfire/board/repository/impl/CustomBoardRepositoryImpl.java
@@ -1,9 +1,16 @@
 package com.ssafy.campfire.board.repository.impl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.campfire.board.domain.Board;
 import com.ssafy.campfire.board.repository.CustomBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static com.ssafy.campfire.board.domain.QBoard.board;
+import static com.ssafy.campfire.category.domain.QCategory.category;
+import static com.ssafy.campfire.user.domain.QUser.user;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,4 +18,15 @@ public class CustomBoardRepositoryImpl implements CustomBoardRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public Optional<Board> getByIdFetchJoin(Long boardId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(board)
+                        .leftJoin(board.user, user).fetchJoin()
+                        .leftJoin(board.category, category).fetchJoin()
+                        .where(board.id.eq(boardId))
+                        .fetchOne()
+        );
+    }
 }
