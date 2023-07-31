d50984400da695a97302aea94b8ded638eab3cda
Park JiHwan <mycook3@naver.com>
2023-07-31 12:28:04 +0900

    feat/지역명, 언어명, 트랙명 리스트 호출 완료

--- src/main/java/com/ssafy/campfire/bootcamp/service/BootTrackService.java
+++ src/main/java/com/ssafy/campfire/bootcamp/service/BootTrackService.java
@@ -2,9 +2,13 @@ package com.ssafy.campfire.bootcamp.service;
 
 import com.ssafy.campfire.bootcamp.domain.BootTrack;
 import com.ssafy.campfire.bootcamp.domain.Bootcamp;
+import com.ssafy.campfire.bootcamp.domain.Region;
 import com.ssafy.campfire.bootcamp.domain.Track;
 import com.ssafy.campfire.bootcamp.dto.request.BootcampRegisterRequestDto;
 import com.ssafy.campfire.bootcamp.repository.BootTrackRepository;
+import com.ssafy.campfire.bootcamp.repository.TrackRepository;
+import com.ssafy.campfire.utils.error.enums.ErrorMessage;
+import com.ssafy.campfire.utils.error.exception.custom.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +22,7 @@ import java.util.Optional;
 @Transactional
 public class BootTrackService {
     private final BootTrackRepository bootTrackRepository;
+    private final TrackRepository trackRepository;
 
     public  List<Track> save(Bootcamp bootcamp, BootcampRegisterRequestDto bootcampRegisterRequestDto){
         List<BootTrack> bootTrackList = bootcampRegisterRequestDto.toBootTrackList(bootcamp);
@@ -35,5 +40,10 @@ public class BootTrackService {
         return trackList;
     }
 
+    public List<Track> getTrackList(){
+        List<Track> trackList = Optional.of(trackRepository.findAll())
+                .orElseThrow(() -> new BusinessException(ErrorMessage.REGION_NOT_FOUND));
 
+        return trackList;
+    }
 }
