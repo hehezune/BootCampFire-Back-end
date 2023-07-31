855c9e6c7be43af30acd23f2dadcbbc145edfe4e
yeonx <ydol2@naver.com>
2023-07-31 11:14:40 +0900

    feat: 댓글 조회 쿼리 구현

--- src/main/java/com/ssafy/campfire/comment/repository/CustomCommentRepository.java
+++ src/main/java/com/ssafy/campfire/comment/repository/CustomCommentRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.campfire.comment.repository;
 
+import com.ssafy.campfire.comment.domain.Comment;
+
+import java.util.List;
+
 public interface CustomCommentRepository {
+    List<Comment> getCommentList(Long boardId);
 }
