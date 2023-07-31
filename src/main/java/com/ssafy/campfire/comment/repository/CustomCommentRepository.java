5c1ae890532bd3575feead1ed0c2ba0cb941c00f
yeonx <ydol2@naver.com>
2023-07-31 09:57:02 +0900

    feat: 댓글 repo 생성

---
+++ src/main/java/com/ssafy/campfire/comment/repository/CustomCommentRepository.java
@@ -0,0 +1,4 @@
+package com.ssafy.campfire.comment.repository;
+
+public interface CustomCommentRepository {
+}
---
+++ src/main/java/com/ssafy/campfire/comment/repository/impl/CustomCommentRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.ssafy.campfire.comment.repository.impl;
+
+import com.ssafy.campfire.comment.repository.CustomCommentRepository;
+
+public class CustomCommentRepositoryImpl implements CustomCommentRepository {
+}
