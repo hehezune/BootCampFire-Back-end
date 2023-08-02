6305027e052d554c875390745045bb0d1a7f6c1a
yeonx <ydol2@naver.com>
2023-08-02 16:35:07 +0900

    fix: 환경변수 test

--- src/test/java/com/ssafy/campfire/reviewLike/dto/request/ReviewLikeAddRequestTest.java
+++ src/test/java/com/ssafy/campfire/reviewLike/dto/request/ReviewLikeAddRequestTest.java
@@ -1,20 +1,20 @@
-//package com.ssafy.campfire.reviewLike.dto.request;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class ReviewLikeAddRequestTest {
-//
-//    @Test
-//    void toReviewLike() {
-//    }
-//
-//    @Test
-//    void user() {
-//    }
-//
-//    @Test
-//    void review() {
-//    }
-//}
+package com.ssafy.campfire.reviewLike.dto.request;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewLikeAddRequestTest {
+
+    @Test
+    void toReviewLike() {
+    }
+
+    @Test
+    void user() {
+    }
+
+    @Test
+    void review() {
+    }
+}
