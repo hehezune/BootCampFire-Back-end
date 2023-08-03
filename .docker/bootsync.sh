fbc8e282ca86206e9e43a9cbed90c488b821b50b
yeonx <ydol2@naver.com>
2023-08-03 16:43:30 +0900

    fix: db 확인

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -9,3 +9,5 @@ sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/def
 ### cicd push를 위한 변화 주기 -> application 로그인권한
 
 ### cicd push를 위한 변화 주기 -> yml 간격
+
+### cicd push를 위한 변화 주기 -> db 확인
