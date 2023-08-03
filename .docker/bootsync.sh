0217d2beb2020c1c1fbe6f41dd60f589cc6d8e28
yeonx <ydol2@naver.com>
2023-08-03 15:20:33 +0900

    fix: docker 권한

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -3,3 +3,5 @@ sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/def
 #sed -i '/if \[ -n \"\$domain\" \]/a     domain=\"$domain dev\"' /usr/share/udhcpc/default.script
 
 ### cicd push를 위한 변화주기 -> ubuntu에 docker설치
+
+### cicd push를 위한 변화 주기 -> ubuntu에 docker권한
