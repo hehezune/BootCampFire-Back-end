4dc01f5acd58055cabdb3998240110c71a19d5a5
yeonx <ydol2@naver.com>
2023-08-03 16:04:32 +0900

    fix: yml간격

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -2,8 +2,10 @@
 sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/default.script
 #sed -i '/if \[ -n \"\$domain\" \]/a     domain=\"$domain dev\"' /usr/share/udhcpc/default.script
 
-### cicd push를 위한 변화주기 -> ubuntu에 docker설치
+### cicd push를 위한 변화 주기 -> ubuntu에 docker설치
 
 ### cicd push를 위한 변화 주기 -> ubuntu에 docker권한
 
 ### cicd push를 위한 변화 주기 -> application 로그인권한
+
+### cicd push를 위한 변화 주기 -> yml 간격
