24dfb13fb5b50265e2726e662d56b14445d9498a
yeonx <ydol2@naver.com>
2023-08-03 15:12:47 +0900

    fix: docker

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/default.script
 #sed -i '/if \[ -n \"\$domain\" \]/a     domain=\"$domain dev\"' /usr/share/udhcpc/default.script
+
+### cicd push를 위한 변화주기 -> ubuntu에 docker설치
