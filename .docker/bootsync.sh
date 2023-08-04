3bf1337c45703e231f4e2da5c75b35f48db6777d
yeonx <ydol2@naver.com>
2023-08-04 09:25:47 +0900

    fix: port 추가

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -21,3 +21,5 @@ sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/def
 ### cicd push를 위한 변화 주기 -> application database path
 
 ### cicd push를 위한 변화 주기 -> application database path - 스키마까지
+
+### cicd push를 위한 변화 주기 -> ec2 3306 port 추가
