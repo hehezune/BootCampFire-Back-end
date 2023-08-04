770b2a9b001cd9be51ee59e4e6833b640908e93f
yeonx <ydol2@naver.com>
2023-08-04 09:10:07 +0900

    fix: 환경변수 url

--- .docker/bootsync.sh
+++ .docker/bootsync.sh
@@ -17,3 +17,5 @@ sed -i '/if \[ -n \"\$dns\" \]/a     dns=\"$dns 8.8.8.8\"' /usr/share/udhcpc/def
 ### cicd push를 위한 변화 주기 -> mvc 추가
 
 ### cicd push를 위한 변화 주기 -> application ddl option
+
+### cicd push를 위한 변화 주기 -> application database path
