33a00f9f536efaa08b4a382c7f24343e2c7440bb
Park JiHwan <mycook3@naver.com>
2023-08-07 13:17:41 +0900

    feat/로그인 성공 후 프론트로 리다이렉트 추가

--- src/main/java/com/ssafy/campfire/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/ssafy/campfire/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -6,6 +6,8 @@ import com.ssafy.campfire.user.domain.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,16 +23,16 @@ import java.io.IOException;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
-
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("OAuth2 Login 성공!");
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-        loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
+        loginSuccess(request, response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
     }
 
     // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private void loginSuccess(HttpServletRequest request,HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
         String accessToken = jwtService.createAccessToken(oAuth2User.getNickname());
         String refreshToken = jwtService.createRefreshToken();
         response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
@@ -38,5 +40,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         jwtService.sendAccessAndRefreshToken(response, "Bearer " +accessToken, "Bearer " +refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getNickname(), refreshToken);
+
+        redirectStrategy.sendRedirect(request, response,"localhost:3000/login/info");
+
     }
 }
